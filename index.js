5f5bf5499905536359b32adde77a98ee485ec530
scr6w <playerzone06@gmail.com>
2024-05-30 14:02:44 -0300

    chore: Update express.static to use path.join for setting static file directory

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname, "views"));
 //i18n4e.default.init(app, {
