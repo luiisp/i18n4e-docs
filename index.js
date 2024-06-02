010b8bdba444274eb361fecdda6ada9d3a2a9562
scr6w <playerzone06@gmail.com>
2024-06-02 01:07:01 -0300

    chore: Update i18n4e package to version 0.3.3 and fix image path in nav.ejs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-//const { i18n4e } = require("i18n4e");
+const { i18n4e } = require("i18n4e");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname, "views"));
+console.log(path.join(__dirname, "locales"));
 i18n4e.init(app, {
   serverSideTranslation: true,
+  options:{
+    langsFolder: path.join(__dirname, "locales"),
+  }
 });
 
 app.get("/", (req, res) => {
