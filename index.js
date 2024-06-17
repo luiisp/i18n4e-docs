e996625541afb952aaa9751773c2e44c73d516b7
scr6w <playerzone06@gmail.com>
2024-06-17 03:55:06 -0300

    refactor: migrate codebase to ES Modules (ESM)

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { i18n4e } from 'i18n4e';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { i18n4e } = require("i18n4e");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
-app.set("views",path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 i18n4e.init(app, {
-  langsFolder: path.join(__dirname, "_locales"),
-  enableClient:true,
-  useLangSession:true,
-  i18n4eDefaultSession:true
-  
-
+  langsFolder: path.join(__dirname, '_locales'),
+  enableClient: true,
+  useLangSession: true,
+  i18n4eDefaultSession: true
 });
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/docs", (req, res) => {
-  res.render("docs/docs");
+app.get('/docs', (req, res) => {
+  res.render('docs/docs');
 });
 
-app.get("/docs/cst", (req, res) => {
-  res.render("docs/cst");
+app.get('/docs/cst', (req, res) => {
+  res.render('docs/cst');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Started on http://localhost:${PORT}`);
